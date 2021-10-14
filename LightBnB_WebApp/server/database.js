825fc675ab894ae2c45f1add2daf79cbf03a7328
chazdean <chaz.dean@shopify.com>
2021-10-14 00:02:33 +0000

    modify getAllReservations to use SQL

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -96,7 +96,21 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(`
+      SELECT properties.*, reservations.start_date, reservations.end_date
+      FROM reservations
+      JOIN properties ON property_id = properties.id
+      WHERE guest_id = $1
+      LIMIT $2
+      `, [guest_id, limit])
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 exports.getAllReservations = getAllReservations;
 
