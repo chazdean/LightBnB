61ee244ffb544a3b945b09bdc83c881ec84c38c2
chazdean <chaz.dean@shopify.com>
2021-10-15 01:07:01 +0000

    add make reservation functionality

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -114,6 +114,18 @@ const getAllReservations = function(guest_id, limit = 10) {
 };
 exports.getAllReservations = getAllReservations;
 
+const addReservation = function(reservation) {
+  /*
+   * Adds a reservation from a specific user to the database
+   */
+  return pool.query(`
+    INSERT INTO reservations (start_date, end_date, property_id, guest_id)
+    VALUES ($1, $2, $3, $4) RETURNING *;
+  `, [reservation.start_date, reservation.end_date, reservation.property_id, reservation.guest_id])
+    .then(res => res.rows[0]);
+};
+
+exports.addReservation = addReservation;
 /// Properties
 
 /**
